a2ed5971e9dd8ce61a45f4436809aba77b0ec2d0
umairriaz789 <49087501+umairriaz789@users.noreply.github.com>
2020-11-26 19:46:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>My Name is Umair Riaz and I'm the Student of BS Computer Sciences.  </h1>
-        <h2>My University name is UET Lahore.</h2>
-        <br />
         <msg counter={count}/>
         <br />
         <button onclick={
