d2904c63023b86c3113992ad57bdbb8a5b42e6be
umairriaz789 <49087501+umairriaz789@users.noreply.github.com>
2020-11-26 19:42:06 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,14 @@ import { useState } from 'react';
 export default function App() {
   let [count,setcount]=useState(1)
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My Name is Umair Riaz and I'm the Student of BS Computer Sciences.  </h1>
-        <h2>My University name is UET Lahore.</h2>
-        <br />
-        <msg counter={count}/>
+    <div >
+      <msg counter={count}/>
         <br />
         <button onclick={
           ()=> setcount(++count)
         }>
           update counter
         </button>
-
-
-   
-      </header>
     </div>
   );
 }
