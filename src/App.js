a67bcab91cee5636546152f7cb7aa3ce4b2d14d6
umairriaz789 <49087501+umairriaz789@users.noreply.github.com>
2020-11-26 18:35:25 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-
+import {msg} from './msg.js';
+import { useState } from 'react';
 function App() {
+  let [count,setcount]=useState()
   return (
     <div className="App">
       <header className="App-header">
         <h1>My Name is Umair Riaz and I'm the Student of BS Computer Sciences.  </h1>
         <h2>My University name is UET Lahore.</h2>
+        <br />
+        <msg />
+        <br />
+        <button onclick={
+          ()=> setcount(++count)
+        }>
+          update counter
+        </button>
+
+
    
       </header>
     </div>
