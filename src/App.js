2162df624f5e0dd9bee9f62b33b2015ab75a2e91
umairriaz789 <49087501+umairriaz789@users.noreply.github.com>
2020-11-26 16:59:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>My Name is Umair Riaz and I'm the Student of BS Computer Sciences and My University name is UET Lahore.  </h1>
+        <h1>My Name is Umair Riaz and I'm the Student of BS Computer Sciences.  </h1>
+        <h2>My University name is UET Lahore.</h2>
    
       </header>
     </div>
