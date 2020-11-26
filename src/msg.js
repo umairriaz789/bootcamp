e5196dc3f7741e65889c239bc1a8684bde1025d5
umairriaz789 <49087501+umairriaz789@users.noreply.github.com>
2020-11-26 20:28:13 +0500

    Update msg.js

--- src/msg.js
+++ src/msg.js
@@ -1,5 +1,5 @@
 import React from 'react';
-export function msg (props) {
+export default function msg (props) {
     return(
         <h3>the value of counter variable is: {props.counter} </h3>
     )
